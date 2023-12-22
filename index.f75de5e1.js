3634e10a57676ed6cc2f439dd4a39ee4171ce641
Oksana Vasyliuk <oksana.vasyliuk13@gmail.com>
2023-12-22 14:30:28 +0100

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,18 @@
+"use strict";
+const inputs = [
+    ...document.querySelectorAll("input")
+];
+inputs.forEach((input)=>{
+    const label = document.createElement("label");
+    label.className = "field-label";
+    label.setAttribute("for", input.id);
+    label.textContent = splitCamelCase(input.name);
+    input.setAttribute("placeholder", splitCamelCase(input.name));
+    input.parentElement.append(label);
+});
+function splitCamelCase(camelWords) {
+    const splited = camelWords.replace(/([a-z])([A-Z])/g, "$1 $2");
+    return splited[0].toUpperCase() + splited.slice(1);
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
